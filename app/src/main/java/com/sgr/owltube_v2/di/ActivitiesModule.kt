f5f78c0cdd7ab1f836e54ae8d1a182bbe9f12384
tomohiko sato <ts@tomohikos-MacBook-Pro.local>
2017-09-06 02:43:51 +0900

    Fra

--- app/src/main/java/com/sgr/owltube_v2/di/ActivitiesModule.kt
+++ app/src/main/java/com/sgr/owltube_v2/di/ActivitiesModule.kt
@@ -1,6 +1,7 @@
 package com.sgr.owltube_v2.di
 
 import com.sgr.owltube_v2.frontend.MainActivity
+import com.sgr.owltube_v2.frontend.top.TopFragment
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 
@@ -9,4 +10,7 @@ abstract class ActivitiesModule {
 
     @ContributesAndroidInjector
     abstract fun provideMainActivityInjector(): MainActivity
+
+    @ContributesAndroidInjector
+    abstract fun provideTopFragmentInjector(): TopFragment
 }
--- app/src/main/java/com/sgr/owltube_v2/frontend/top/TopFragment.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/top/TopFragment.kt
@@ -4,15 +4,21 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.sgr.owltube_v2.R
 import com.sgr.owltube_v2.dummy.DummyContent
 import com.sgr.owltube_v2.dummy.DummyContent.DummyItem
+import dagger.android.support.AndroidSupportInjection
+import retrofit2.Retrofit
+import javax.inject.Inject
 
 class TopFragment : Fragment() {
     private lateinit var listener: OnListFragmentInteractionListener
+
+    @Inject lateinit var retrofit : Retrofit
 
     companion object {
         fun newInstance(): TopFragment {
@@ -22,12 +28,14 @@ class TopFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        Log.d("fugafuga", retrofit.toString())
         return (inflater.inflate(R.layout.fragment_item_list, container, false) as RecyclerView).apply {
             adapter = TopItemRecyclerViewAdapter(DummyContent.ITEMS, listener)
         }
     }
 
     override fun onAttach(context: Context) {
+        AndroidSupportInjection.inject(this)
         super.onAttach(context)
         if (context is OnListFragmentInteractionListener) {
             listener = context
