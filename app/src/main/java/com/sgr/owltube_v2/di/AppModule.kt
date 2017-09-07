75f106bbf61f0a8f031bc3c609188457afcd568a
tomohiko sato <ts@tomohikos-MacBook-Pro.local>
2017-09-07 19:13:23 +0900

    ok

--- app/src/main/java/com/sgr/owltube_v2/di/AppModule.kt
+++ app/src/main/java/com/sgr/owltube_v2/di/AppModule.kt
@@ -8,6 +8,7 @@ import dagger.Provides
 import io.reactivex.schedulers.Schedulers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
+import okhttp3.logging.HttpLoggingInterceptor.Level.*
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -24,8 +25,7 @@ class AppModule {
     @Singleton
     @Provides
     fun providesOkHttp(): OkHttpClient = OkHttpClient.Builder()
-            .addInterceptor(HttpLoggingInterceptor()
-                    .apply { level = HttpLoggingInterceptor.Level.BODY })
+            .addInterceptor(HttpLoggingInterceptor().apply { level = BODY })
             .build()
 
 
