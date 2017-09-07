80fefbbd90bcc0ce8c5369d94d54f077fce51628
tomohiko sato <ts@tomohikos-MacBook-Pro.local>
2017-09-07 22:08:33 +0900

    hei

--- app/src/main/java/com/sgr/owltube_v2/infra/webapi/response/popular/Snippet.kt
+++ app/src/main/java/com/sgr/owltube_v2/infra/webapi/response/popular/Snippet.kt
@@ -1,3 +1,11 @@
 package com.sgr.owltube_v2.infra.webapi.response.popular
 
-data class Snippet(val publishedAt: String, val channelId: String, val title: String, val description: String, val thumbnails: Thumbnails, val channelTitle: String, val categoryId: String, val liveBroadcastContent: String, val localized: Localized) 
+data class Snippet(val publishedAt: String,
+                   val channelId: String,
+                   val title: String,
+                   val description: String,
+                   val thumbnails: Thumbnails,
+                   val channelTitle: String,
+                   val categoryId: String,
+                   val liveBroadcastContent: String,
+                   val localized: Localized)
