999fd3fb52e020bf0bd45095d04b271fe1c56c55
tomohiko sato <ts@tomohikos-MacBook-Pro.local>
2017-09-09 01:00:52 +0900

    duration

--- app/src/main/java/com/sgr/owltube_v2/App.kt
+++ app/src/main/java/com/sgr/owltube_v2/App.kt
@@ -2,6 +2,7 @@ package com.sgr.owltube_v2
 
 import android.app.Activity
 import android.app.Application
+import com.jakewharton.threetenabp.AndroidThreeTen
 import com.sgr.owltube_v2.di.DaggerAppComponent
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasActivityInjector
@@ -17,5 +18,6 @@ class App : Application(), HasActivityInjector {
         super.onCreate()
         DaggerAppComponent.create()
                 .inject(this)
+        AndroidThreeTen.init(this);
     }
 }
--- app/src/main/java/com/sgr/owltube_v2/domain/Video.kt
+++ app/src/main/java/com/sgr/owltube_v2/domain/Video.kt
@@ -1,6 +1,6 @@
 package com.sgr.owltube_v2.domain
 
-import org.threeten.bp.Duration
+import org.threeten.bp.*
 
 
 data class Video(val id: String,
@@ -8,8 +8,24 @@ data class Video(val id: String,
                  val channel: Channel,
                  val viewCount: String,
                  val thumbnailUrl: String,
-                 val publishedAt: String,
+                 val _publishedAt: String,
                  val _duration: String) {
+
+    val publishedAt: String
+        get() {
+            val period = Period.between(OffsetDateTime.parse(_publishedAt).toLocalDate(), LocalDate.now())
+
+            if (period.years > 0) return String.format("%d年前", period.years)
+            if (period.months > 0) return String.format("%dヶ月前", period.months)
+            if (period.days >= 7) return String.format("%d週間前", period.days / 7)
+            if (period.days > 1) return String.format("%d日前", period.days)
+
+            val duration = Duration.between(OffsetDateTime.parse(_publishedAt).toLocalDateTime(), LocalDateTime.now())
+            if (duration.toHours() > 0) return String.format("%d時間前", duration.toHours())
+            if (duration.toMinutes() > 0) return String.format("%d分前", duration.toMinutes())
+
+            return String.format("%d秒前", duration.seconds)
+        }
 
     val duration: String
         get() {
