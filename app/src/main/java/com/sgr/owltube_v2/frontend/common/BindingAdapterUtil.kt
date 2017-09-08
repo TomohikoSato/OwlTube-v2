771d27b0b2dd7205113ab67b59f589e6394d7208
tomohiko sato <ts@tomohikos-MacBook-Pro.local>
2017-09-08 11:03:15 +0900

    centercrop glideapp

--- app/src/main/java/com/sgr/owltube_v2/frontend/common/BindingAdapterUtil.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/common/BindingAdapterUtil.kt
@@ -3,10 +3,20 @@ package com.sgr.owltube_v2.frontend.common
 import android.databinding.BindingAdapter
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions.circleCropTransform
+import com.sgr.owltube_v2.R
 
 @BindingAdapter("bind:imageUrl")
 fun setImageUrl(imageView: ImageView, imageUrl: String) {
-    Glide.with(imageView.context)
+    GlideApp.with(imageView.context)
             .load(imageUrl)
+            .into(imageView);
+}
+
+@BindingAdapter("bind:circleCropImageUrl")
+fun setCircleCropImageUrl(imageView: ImageView, imageUrl: String) {
+    GlideApp.with(imageView.context)
+            .load(imageUrl)
+            .apply(circleCropTransform())
             .into(imageView);
 }
