526b3f79da80de5d3e44b27fd5ae842298966c6f
tomohiko sato <ts@tomohikos-MacBook-Pro.local>
2017-09-08 23:12:02 +0900

    duration

--- app/src/main/java/com/sgr/owltube_v2/domain/Video.kt
+++ app/src/main/java/com/sgr/owltube_v2/domain/Video.kt
@@ -1,9 +1,21 @@
 package com.sgr.owltube_v2.domain
 
+import org.threeten.bp.Duration
+
+
 data class Video(val id: String,
                  val title: String,
                  val channel: Channel,
                  val viewCount: String,
                  val thumbnailUrl: String,
                  val publishedAt: String,
-                 val duration: String)
+                 val _duration: String) {
+
+    val duration: String
+        get() {
+            val d = Duration.parse(_duration)
+            return if (d.compareTo(Duration.ofHours(1)) >= 0)
+                String.format("%d:%02d:%02d", d.toHours(), d.toMinutes() % 60, d.seconds % 60) else
+                String.format("%02d:%02d", d.toMinutes() % 60, d.seconds % 60)
+        }
+}
