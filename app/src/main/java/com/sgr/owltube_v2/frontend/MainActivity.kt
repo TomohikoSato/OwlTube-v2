e5f5ed8879acfd377cd5a081981ce0cbefe0b2a9
tomohiko sato <tomohikosato1@gmail.com>
2017-09-11 10:42:07 +0900

    setting ignore

--- app/src/main/java/com/sgr/owltube_v2/frontend/MainActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/MainActivity.kt
@@ -9,7 +9,6 @@ import com.sgr.owltube_v2.frontend.common.disableShiftingMode
 import com.sgr.owltube_v2.frontend.mypage.MyPageFragment
 import com.sgr.owltube_v2.frontend.player.PlayerActivity
 import com.sgr.owltube_v2.frontend.search.SearchFragment
-import com.sgr.owltube_v2.frontend.setting.SettingFragment
 import com.sgr.owltube_v2.frontend.top.TopFragment
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -47,12 +46,6 @@ class MainActivity : DaggerAppCompatActivity(), TopFragment.OnTopFragmentListIte
             R.id.navigation_mypage -> {
                 supportFragmentManager.beginTransaction()
                         .replace(R.id.container, MyPageFragment.newInstance())
-                        .commit()
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_settings -> {
-                supportFragmentManager.beginTransaction()
-                        .replace(R.id.container, SettingFragment.newInstance())
                         .commit()
                 return@OnNavigationItemSelectedListener true
             }
