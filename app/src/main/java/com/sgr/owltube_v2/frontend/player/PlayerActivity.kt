8e4ced26c574ed5cefee1cd7ee6cfb10460128d6
tomohiko sato <tomohikosato1@gmail.com>
2017-09-12 12:14:12 +0900

    fullscreen

--- app/src/main/java/com/sgr/owltube_v2/frontend/player/PlayerActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/player/PlayerActivity.kt
@@ -1,16 +1,19 @@
 package com.sgr.owltube_v2.frontend.player
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import com.pierfrancescosoffritti.youtubeplayer.AbstractYouTubeListener
+import com.pierfrancescosoffritti.youtubeplayer.YouTubePlayerFullScreenListener
 import com.pierfrancescosoffritti.youtubeplayer.YouTubePlayerView
 import com.sgr.owltube_v2.R
 import com.sgr.owltube_v2.domain.Video
 
 class PlayerActivity : AppCompatActivity() {
-
     companion object {
         private val KEY_INTENT_EXTRA_VIDEO: String = "key_intent_extra_video"
 
@@ -27,11 +30,50 @@ class PlayerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_player)
         val video = intent.getSerializableExtra(KEY_INTENT_EXTRA_VIDEO) as Video
 
-        val youtubePlayer = findViewById<YouTubePlayerView>(R.id.youtube_player)
-        youtubePlayer.initialize(object : AbstractYouTubeListener() {
+        val youtubePlayerView = findViewById<YouTubePlayerView>(R.id.youtube_player)
+        youtubePlayerView.initialize(object : AbstractYouTubeListener() {
             override fun onReady() {
-                youtubePlayer.loadVideo(video.id, 0F)
+                youtubePlayerView.loadVideo(video.id, 0F)
             }
         }, false)
+
+        youtubePlayerView.addFullScreenListener(object : YouTubePlayerFullScreenListener {
+            private val fullScreenManager = FullScreenManager(this@PlayerActivity)
+            override fun onYouTubePlayerEnterFullScreen() {
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                fullScreenManager.enterFullScreen()
+            }
+
+            override fun onYouTubePlayerExitFullScreen() {
+                requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                fullScreenManager.exitFullScreen()
+            }
+        })
+    }
+
+    class FullScreenManager(val activity: Activity) {
+        fun enterFullScreen() {
+            hideSystemUI(activity.window.decorView)
+        }
+
+        fun exitFullScreen() {
+            showSystemUI(activity.window.decorView)
+        }
+
+        // hides the system bars.
+        private fun hideSystemUI(decorView: View) {
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                    View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
+
+        // This snippet shows the system bars.
+        private fun showSystemUI(mDecorView: View) {
+            mDecorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
     }
 }
+
