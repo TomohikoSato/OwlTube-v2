425d32fcfa1fddef1b04157803b520b5a8485afe
tomohiko sato <tomohikosato1@gmail.com>
2017-09-18 16:25:39 +0900

    search

--- app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchFragment.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchFragment.kt
@@ -1,58 +1,29 @@
 package com.sgr.owltube_v2.frontend.search
 
 import android.content.Context
-import android.net.Uri
 import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.sgr.owltube_v2.R
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [SearchFragment.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     private var listener: OnFragmentInteractionListener? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        if (arguments != null) {
-            param1 = arguments.getString(ARG_PARAM1)
-            param2 = arguments.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(inflater: LayoutInflater?,
+                              container: ViewGroup?,
+                              @Nullable savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_search, container, false)
     }
 
-    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater!!.inflate(R.layout.fragment_search, container, false)
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    fun onButtonPressed(uri: Uri) {
-        if (listener != null) {
-            listener!!.onFragmentInteraction(uri)
-        }
-    }
-
-    override fun onAttach(context: Context?) {
+    override fun onAttach(context: Context) {
         super.onAttach(context)
         if (context is OnFragmentInteractionListener) {
             listener = context
         } else {
-
+            //throw RuntimeException(context.toString() + " must implement OnFragmentInteractionListener")
         }
     }
 
@@ -61,37 +32,11 @@ class SearchFragment : Fragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html) for more information.
-     */
     interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onFragmentInteraction(uri: Uri)
+        fun onFragmentInteraction()
     }
 
     companion object {
-        // TODO: Rename parameter arguments, choose names that match
-        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-        private val ARG_PARAM1 = "param1"
-        private val ARG_PARAM2 = "param2"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        fun newInstance(): SearchFragment {
-            return SearchFragment()
-        }
+        fun newInstance() = SearchFragment()
     }
-}// Required empty public constructor
+}
