b96e531a6848247794de27e6acf517674370f1ee
tomohiko sato <tomohikosato1@gmail.com>
2017-09-18 19:27:29 +0900

    うごいた

--- app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchActivity.kt
@@ -1,10 +1,12 @@
 package com.sgr.owltube_v2.frontend.search
 
+import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageButton
+import android.widget.SearchView
 import com.sgr.owltube_v2.R
 
 class SearchActivity : AppCompatActivity() {
@@ -13,6 +15,18 @@ class SearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         findViewById<ImageButton>(R.id.up).setOnClickListener { _ -> finish() }
+        findViewById<SearchView>(R.id.search_view).apply {
+            setSearchableInfo(
+                    (getSystemService(Context.SEARCH_SERVICE) as SearchManager).getSearchableInfo(getComponentName()))
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String): Boolean {
+                    SearchResultActivity.startActivity(this@SearchActivity)
+                    return true
+                }
+
+                override fun onQueryTextChange(p0: String): Boolean = false
+            })
+        }
     }
 
     companion object {
--- app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchResultActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchResultActivity.kt
@@ -1,5 +1,7 @@
 package com.sgr.owltube_v2.frontend.search
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.sgr.owltube_v2.R
@@ -9,5 +11,11 @@ class SearchResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_result)
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            context.startActivity(Intent(context, SearchResultActivity::class.java))
+        }
     }
 }
