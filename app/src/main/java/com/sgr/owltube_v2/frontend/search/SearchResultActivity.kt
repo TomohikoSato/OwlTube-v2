79fc21eb43a3aff5c7472d58cf77636f5f60456b
tomohiko sato <tomohikosato1@gmail.com>
2017-09-19 01:33:29 +0900

    けんさくけっか

--- app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchResultActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchResultActivity.kt
@@ -3,7 +3,11 @@ package com.sgr.owltube_v2.frontend.search
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
+import android.view.View
 import com.sgr.owltube_v2.R
+import com.sgr.owltube_v2.domain.Video
+import com.sgr.owltube_v2.frontend.player.PlayerActivity
 import dagger.android.support.DaggerAppCompatActivity
 import javax.inject.Inject
 
@@ -13,12 +17,13 @@ class SearchResultActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_search_result)
         val query = intent.getStringExtra(KEY_QUERY)
         searchResultViewModel.search(query)
-/*        findViewById<RecyclerView>(R.id.recycler_view).adapter =
+        findViewById<RecyclerView>(R.id.recycler_view).adapter =
                 SearchResultAdapter(searchResultViewModel.searchResultVideos,
-                        { view: View, relatedVideo: RelatedVideo -> PlayerActivity.startActivity(this, relatedVideo) })*/
+                        { _: View, video: Video -> PlayerActivity.startActivity(this, video) })
     }
 
     companion object {
