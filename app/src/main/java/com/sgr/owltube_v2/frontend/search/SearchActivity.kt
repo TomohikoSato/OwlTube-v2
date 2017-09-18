bca59f13a7eecdec7eb01d2828788c82ecf608a5
tomohiko sato <tomohikosato1@gmail.com>
2017-09-18 18:09:35 +0900

    up finish

--- app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageButton
 import com.sgr.owltube_v2.R
 
 class SearchActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        findViewById<ImageButton>(R.id.up).setOnClickListener { _ -> finish() }
     }
 
     companion object {
---
+++ app/src/main/java/com/sgr/owltube_v2/frontend/search/SearchResultActivity.kt
@@ -0,0 +1,13 @@
+package com.sgr.owltube_v2.frontend.search
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.sgr.owltube_v2.R
+
+class SearchResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result)
+    }
+}
