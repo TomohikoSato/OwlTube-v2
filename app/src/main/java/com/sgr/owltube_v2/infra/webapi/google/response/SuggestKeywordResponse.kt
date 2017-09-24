9e52529105ebd22c36892033e8aa228922f85945
tomohiko sato <tomohikosato1@gmail.com>
2017-09-24 20:30:15 +0900

    hey

--- app/src/main/java/com/sgr/owltube_v2/infra/webapi/google/response/SuggestKeywordResponse.kt
+++ app/src/main/java/com/sgr/owltube_v2/infra/webapi/google/response/SuggestKeywordResponse.kt
@@ -6,25 +6,17 @@ import org.simpleframework.xml.ElementList
 import org.simpleframework.xml.Root
 
 @Root(name = "toplevel", strict = false)
-class SuggestKeywordResponse() {
-
-    @set:ElementList(inline = true, name = "CompleteSuggestion")
-    @get:ElementList(inline = true, name = "CompleteSuggestion")
-    var suggests: List<CompleteSuggestion>? = null
-
-}
+class SuggestKeywordResponse(@set:ElementList(inline = true, name = "CompleteSuggestion")
+                             @get:ElementList(inline = true, name = "CompleteSuggestion")
+                             var suggests: List<CompleteSuggestion>? = null
+)
 
 @Root(name = "CompleteSuggestion", strict = false)
-class CompleteSuggestion() {
-
-    @set:Element
-    @get:Element
-    var suggestion: Suggestion? = null
-}
+class CompleteSuggestion(@set:Element
+                         @get:Element
+                         var suggestion: Suggestion? = null)
 
 @Root(name = "Suggestion", strict = false)
-class Suggestion {
-    @set:Attribute(name = "data")
-    @get:Attribute(name = "data")
-    var data: String? = null
-}
+class Suggestion(@set:Attribute(name = "data")
+                 @get:Attribute(name = "data")
+                 var data: String? = null)
