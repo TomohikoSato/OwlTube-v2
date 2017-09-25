f2f0f2ac6199c0fbb6d7fb8757083c853da2eb3b
tomohiko sato <tomohikosato1@gmail.com>
2017-09-25 16:46:46 +0900

    shadow

---
+++ app/src/main/java/com/sgr/owltube_v2/frontend/setting/OSSActivity.kt
@@ -0,0 +1,24 @@
+package com.sgr.owltube_v2.frontend.setting
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.webkit.WebView
+import com.sgr.owltube_v2.R
+
+class OSSActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_oss)
+
+        findViewById<WebView>(R.id.web_view).loadUrl("file:///android_asset/open_source_license.html")
+    }
+
+    companion object {
+        fun startActivity(context: Context) {
+            context.startActivity(Intent(context, OSSActivity::class.java))
+        }
+    }
+}
--- app/src/main/java/com/sgr/owltube_v2/frontend/setting/SettingActivity.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/setting/SettingActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 import com.sgr.owltube_v2.R
 
 class SettingActivity : AppCompatActivity() {
@@ -11,6 +12,11 @@ class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setting)
+        setSupportActionBar(findViewById(R.id.tool_bar))
+
+        findViewById<TextView>(R.id.text_open_source_license).setOnClickListener {
+            OSSActivity.startActivity(this@SettingActivity)
+        }
     }
 
     companion object {
