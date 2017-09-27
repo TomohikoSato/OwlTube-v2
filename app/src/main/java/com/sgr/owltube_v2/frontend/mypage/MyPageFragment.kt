3d3bc3c5d9fd17256f24e010269dbf4785250de9
tomohiko sato <tomohikosato1@gmail.com>
2017-09-27 19:27:36 +0900

    swiperefresh

--- app/src/main/java/com/sgr/owltube_v2/frontend/mypage/MyPageFragment.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/mypage/MyPageFragment.kt
@@ -1,12 +1,12 @@
 package com.sgr.owltube_v2.frontend.mypage
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageButton
 import com.sgr.owltube_v2.R
+import com.sgr.owltube_v2.databinding.FragmentMyPageBinding
 import com.sgr.owltube_v2.domain.Video
 import com.sgr.owltube_v2.frontend.common.ScrollToTop
 import com.sgr.owltube_v2.frontend.player.PlayerActivity
@@ -16,22 +16,24 @@ import javax.inject.Inject
 
 class MyPageFragment : DaggerFragment(), ScrollToTop {
 
-    @Inject lateinit var viewModel: MyPageViewModel
+    @Inject lateinit var myPageViewModel: MyPageViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
-        viewModel.fetchRecentlyWatchedVideos()
 
-        return inflater.inflate(R.layout.fragment_my_page, container, false).apply {
-            findViewById<RecyclerView>(R.id.recycler_view).adapter =
-                    MyPageAdapter(viewModel.items, { _: View, video: Video ->
-                        PlayerActivity.startActivity(this@MyPageFragment.context, video)
-                    })
+        val binding: FragmentMyPageBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_my_page, container, false)
+        myPageViewModel.fetchRecentlyWatchedVideos()
 
-            findViewById<ImageButton>(R.id.setting).setOnClickListener { _: View ->
+        return binding.apply {
+            viewModel = myPageViewModel
+            recyclerView.adapter = MyPageAdapter(myPageViewModel.items, { _: View, video: Video ->
+                PlayerActivity.startActivity(this@MyPageFragment.context, video)
+            })
+            setting.setOnClickListener { _: View ->
                 SettingActivity.startActivity(this@MyPageFragment.context)
             }
-        }
+            swipeRefresh.setEnabled(false)
+        }.root
     }
 
     companion object {
--- app/src/main/java/com/sgr/owltube_v2/frontend/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/sgr/owltube_v2/frontend/mypage/MyPageViewModel.kt
@@ -1,6 +1,7 @@
 package com.sgr.owltube_v2.frontend.mypage
 
 import android.databinding.ObservableArrayList
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableList
 import com.sgr.owltube_v2.frontend.common.adapter.delegate.core.AdapterItem
 import com.sgr.owltube_v2.infra.repository.VideoRepository
@@ -10,13 +11,18 @@ import javax.inject.Inject
 
 class MyPageViewModel @Inject constructor(private val videoRepository: VideoRepository) {
     val items: ObservableList<AdapterItem> = ObservableArrayList<AdapterItem>()
+    val isRefreshing: ObservableBoolean = ObservableBoolean(false)
 
     fun fetchRecentlyWatchedVideos() {
         items.clear()
         items.add(0, MyPageAdapter.HeaderItem())
+        isRefreshing.set(true)
         videoRepository.fetchRecentlyWatchedVideos()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({ videos -> items.addAll(videos) })
+                .subscribe({ videos ->
+                    items.addAll(videos)
+                    isRefreshing.set(false)
+                })
     }
 }
