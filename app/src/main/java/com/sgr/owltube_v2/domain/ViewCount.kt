5a9d1c30fdbb36719502c78cdc4356760e5ac811
tomohiko sato <tomohikosato1@gmail.com>
2018-03-18 19:42:15 +0900

    テスト書いた

--- app/src/main/java/com/sgr/owltube_v2/domain/ViewCount.kt
+++ app/src/main/java/com/sgr/owltube_v2/domain/ViewCount.kt
@@ -1,13 +1,16 @@
 package com.sgr.owltube_v2.domain
 
+import java.text.NumberFormat
+
 /** 視聴回数 */
 data class ViewCount(private val value: Int) {
     /**
-     * 万単位がある
+     * 億単位、万単位がある
      * 3桁目にカンマがある
      */
     fun show(): String = when (value) {
-        in 10000 until Int.MAX_VALUE -> (value / 10000).putComma() + "万回"
+        in 10000 * 10000..Int.MAX_VALUE -> (value / (10000 * 10000)).putComma() + "億回"
+        in 10000 until 10000 * 10000 -> (value / 10000).putComma() + "万回"
         in 0 until 10000 -> value.putComma() + "回"
         in Int.MIN_VALUE until 0 -> error("invalid value: ${value}")
         else -> error("unreachable")
@@ -17,12 +20,7 @@ data class ViewCount(private val value: Int) {
      * ３桁目にカンマを追加
      * e.g. 1000 -> 1,000
      */
-    private fun Int.putComma(): String =
-            this.toString()
-                    .reversed()
-                    .chunked(3)
-                    .joinToString(",")
-                    .reversed()
+    private fun Int.putComma(): String = NumberFormat.getInstance().format(this)
 }
 
 
--- app/src/test/java/com/sgr/owltube_v2/domain/ViewCountTest.kt
+++ app/src/test/java/com/sgr/owltube_v2/domain/ViewCountTest.kt
@@ -3,24 +3,52 @@ package com.sgr.owltube_v2.domain
 import junitparams.JUnitParamsRunner
 import junitparams.Parameters
 import org.junit.Assert.assertEquals
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 
 @RunWith(JUnitParamsRunner::class)
 class ViewCountTest {
+
+    @get:Rule
+    var thrown = ExpectedException.none()
+
     @Test
     @Parameters(method = "provideViewCount")
-    fun show(expected: String, viewCountValue: Int) {
+    fun show_valid(expected: String, viewCountValue: Int) {
         assertEquals(expected, ViewCount(viewCountValue).show())
     }
 
     fun provideViewCount(): Array<Array<out Any>> =
             arrayOf(
+                    arrayOf("21億回", Int.MAX_VALUE), //カンナムスタイル31億回
+                    arrayOf("1億回", 100000000),
+                    arrayOf("9,999万回", 99999999),
+                    arrayOf("4,952万回", 49521346),
+                    arrayOf("36万回", 362940),
                     arrayOf("1万回", 10000),
                     arrayOf("9,999回", 9999),
-                    arrayOf("103回", 103),
+                    arrayOf("583回", 583),
                     arrayOf("100回", 100),
+                    arrayOf("28回", 28),
                     arrayOf("1回", 1),
                     arrayOf("0回", 0)
             )
+
+    @Test
+    @Parameters(method = "provideInvalidViewCount")
+    fun show_invalid(expectedException: Throwable, viewCountValue: Int) {
+        thrown.expect(expectedException::class.java)
+        ViewCount(viewCountValue).show()
+    }
+
+    fun provideInvalidViewCount(): Array<Array<out Any>> =
+            arrayOf(
+                    arrayOf(IllegalStateException(), -1),
+                    arrayOf(IllegalStateException(), Int.MIN_VALUE)
+            )
 }
+
+
+
